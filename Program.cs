d5bb30dc6451f5ffd92ec165a6e3593a60c79d73
Justin <cabrerajustin13@gmail.com>
2026-02-14 12:25:57 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleProgramGananFeb14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
