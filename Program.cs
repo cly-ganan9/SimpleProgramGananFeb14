5a0e386cb384a8a099396923b1807fb2bad5b6f3
Justin <cabrerajustin13@gmail.com>
2026-02-14 12:27:36 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SimpleProgramGananFeb14
+﻿using System;
+
+namespace SimpleProgramGananFeb14
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("PUPBC SCHOOL ACCOUNT");
+
+            string fname = "Clyza";
+            string lname = "Ganan";
+            string password = "092106";
+
+            Console.Write("Enter first name: ");
+            string fnameInput = Console.ReadLine();
+            Console.Write("Enter last name: ");
+            string lnameInput = Console.ReadLine();
+            Console.Write("Enter password: ");
+            string passwordInput = Console.ReadLine();
+
+            if (fnameInput == fname && lnameInput == lname && passwordInput == password)
+            {
+                Console.WriteLine("Welcome to PUPBC!");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect details. Try Again.");
+            }
         }
     }
 }
